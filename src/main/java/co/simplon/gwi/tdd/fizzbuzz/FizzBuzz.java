97271aec4bc0c172b696ecc5d4c858b6af736322
gabrielW <gabriel.wisniewski@gmail.com>
2018-07-03 09:59:28 +0200

    corrected tests

--- src/main/java/co/simplon/gwi/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/co/simplon/gwi/tdd/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,24 @@ package co.simplon.gwi.tdd.fizzbuzz;
 
 public class FizzBuzz {
 	public static String fizzBuzz(int min, int max) throws IllegalArgumentException {
-		
+
 		if (min > max) {
 			throw new IllegalArgumentException("max should be superior at min");
+		} else if (min == max) {
+			String result = "";
+			if (min % 15 == 0) {
+				result += "FizzBuzz";
+			} else if (min % 3 == 0) {
+				result += "Fizz";
+			} else if (min % 5 == 0) {
+				result += "Buzz";
+			} else {
+				result += Integer.toString(min);
+			}
+
+			return result;
 		}
-		
+
 		return null;
 	}
 }
--- src/test/java/co/simplon/gwi/tdd/fizzbuzz/TestFizzBuzz.java
+++ src/test/java/co/simplon/gwi/tdd/fizzbuzz/TestFizzBuzz.java
@@ -9,9 +9,26 @@ public class TestFizzBuzz {
 	public void shouldReturnAnIllegalArgumentExceptionIfMinIsSuperiorToMax() {
 	    FizzBuzz.fizzBuzz(3, 2);
 	}
+	
 	@Test
 	public void shouldReturnMinIfMinAndMaxAreTheSame()
 	{
 		assertEquals("2", FizzBuzz.fizzBuzz(2, 2));		
 	}
+	
+	@Test
+	public void shouldReturnFizzIfDivisibleBy3() {
+		assertEquals("Fizz", FizzBuzz.fizzBuzz(3, 3));
+	}
+	
+	@Test
+	public void shouldReturnBuzzIfDivisibleBy5() {
+		assertEquals("Buzz", FizzBuzz.fizzBuzz(5, 5));
+	}
+	
+	@Test
+	public void shouldReturnFizzBuzzIfDivisibleBy15() {
+		assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15, 15));
+	}
+	
 }
